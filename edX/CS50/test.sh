e2d969053e8d71f3ff10542ce042fa1629da4743
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-21 00:28:51 -0400

    Re org

--- edX/CS50/test.sh
+++ edX/CS50/test.sh
@@ -2,3 +2,5 @@
 check50 2014/x/pset1/hello hello.c
 check50 2014/x/pset1/greedy greedy.c
 check50 2014/x/pset1/mario mario.c
+check50 2014/x/pset2/caesar caesar.c
+check50 2014/x/pset2/vigenere vigenere.c
