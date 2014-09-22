166953cfffab4106bd61873dd14d14bf04c57c4f
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-22 16:07:02 -0500

    adding instruction to download pset4.

--- edX/CS50/test.sh
+++ edX/CS50/test.sh
@@ -4,3 +4,4 @@ check50 2014/x/pset1/greedy greedy.c
 check50 2014/x/pset1/mario mario.c
 check50 2014/x/pset2/caesar caesar.c
 check50 2014/x/pset2/vigenere vigenere.c
+#wget http://cdn.cs50.net/2013/fall/lectures/5/m/src5m.zip
