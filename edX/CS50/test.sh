621fee218401f59be4ea488c5b80c43fc3259519
prasannabe2004 <prasannabe2004@gmail.com>
2014-09-23 15:13:13 -0500

    scratch pad.

--- edX/CS50/test.sh
+++ edX/CS50/test.sh
@@ -5,7 +5,7 @@ check50 2014/x/pset1/mario mario.c
 check50 2014/x/pset2/caesar caesar.c
 check50 2014/x/pset2/vigenere vigenere.c
 #wget http://cdn.cs50.net/2013/fall/lectures/5/m/src5m.zip
-
+#wget http://cdn.cs50.net/2013/fall/psets/4/pset4/pset4.zip
 Find the minimum value in the array
 Swap the minimum value with the value in the first position
 Repeat the steps above for the rest of the elements
