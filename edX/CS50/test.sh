49b0835be38d4ae58c51593ef444db540a2ab446
prasannapm <prasannabe2004@gmail.com>
2014-09-27 11:48:16 -0400

    Adding pset5 questions file.

--- edX/CS50/test.sh
+++ edX/CS50/test.sh
@@ -6,7 +6,7 @@ check50 2014/x/pset2/caesar caesar.c
 check50 2014/x/pset2/vigenere vigenere.c
 #wget http://cdn.cs50.net/2013/fall/lectures/5/m/src5m.zip
 #wget http://cdn.cs50.net/2013/fall/psets/4/pset4/pset4.zip
-
+#wget http://cdn.cs50.net/2013/fall/psets/5/pset5/pset5.zip
 
 Find the minimum value in the array
 Swap the minimum value with the value in the first position
