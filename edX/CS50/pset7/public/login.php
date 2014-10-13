d7b19d2c651dd8ad92cf87154bf9fd824b80366b
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-13 17:34:12 -0500

    cosmic changes.

--- edX/CS50/pset7/public/login.php
+++ edX/CS50/pset7/public/login.php
@@ -15,16 +15,13 @@
         {
             apologize("You must provide your password.");
         }
-
         // query database for user
         $rows = query("SELECT * FROM users WHERE username = ?", $_POST["username"]);
-
         // if we found user, check password
         if (count($rows) == 1)
         {
             // first (and only) row
             $row = $rows[0];
-
             // compare hash of user's input against hash that's in database
             if (crypt($_POST["password"], $row["hash"]) == $row["hash"])
             {
@@ -35,9 +32,10 @@
                 redirect("/");
             }
         }
-
-        // else apologize
-        apologize("Invalid username and/or password.");
+        else
+        {
+            apologize("Invalid username and/or password.");
+        }
     }
     else
     {
