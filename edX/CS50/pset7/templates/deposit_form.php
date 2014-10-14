2f8aa6e508bea4377c95f99d953fe48cb3c9e623
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-14 18:18:17 -0500

    textual changes.

--- edX/CS50/pset7/templates/deposit_form.php
+++ edX/CS50/pset7/templates/deposit_form.php
@@ -5,7 +5,7 @@ include 'common.php';
 <form action="deposit.php" method="post">
     <fieldset>
         <div class="control-group">
-            <input class="input-medium" name="deposit" placeholder="Deposit Amount" type="text"/>
+            <input class="input-medium" name="deposit" placeholder="Deposit Cash in $" type="text"/>
         </div>
         <div class="control-group">
             <br/>
