34d3ca4fb8915f682428053b3956040c1727da80
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-14 13:17:46 -0500

    cosmic changes.

--- edX/CS50/pset7/public/index.php
+++ edX/CS50/pset7/public/index.php
@@ -1,7 +1,7 @@
 <?php
     // configuration
     require("../includes/config.php");
-    $rows =	query("SELECT * FROM portfolio WHERE id = ?", $_SESSION["username"]);
+    $rows =	query("SELECT * FROM portfolio WHERE id = ?", $_SESSION["id"]);
 	$portfolio = [];
 	foreach ($rows as $row)	
     {
@@ -10,6 +10,6 @@
         $stock["total"] = $row["shares"] * $stock["price"];
         $portfolio[] = $stock;
     }
-    $users = query("SELECT * FROM users WHERE id = ?", $_SESSION["username"]);
+    $users = query("SELECT * FROM users WHERE id = ?", $_SESSION["id"]);
     render("portfolio.php", ["title" => "Portfolio", "portfolio" => $portfolio, "users" => $users]);
 ?>
