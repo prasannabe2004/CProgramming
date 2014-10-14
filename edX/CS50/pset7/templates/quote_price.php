273457b9801c363a024188e5d66b32063e4ee70e
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-14 13:40:21 -0500

    cosmic changes.

--- edX/CS50/pset7/templates/quote_price.php
+++ edX/CS50/pset7/templates/quote_price.php
@@ -8,4 +8,4 @@
     <li><a href="logout.php"><strong>Log Out</strong></a></li>
 </ul>
 
-A share of <?=$_POST["name"]?> (<?=$_POST["symbol"]?>) costs <strong>$<?=number_format($_POST["price"], 2)?></strong>
+Today's value of <?=$_POST["name"]?> (<?=$_POST["symbol"]?>) is <strong>$<?=number_format($_POST["price"], 2)?></strong>
