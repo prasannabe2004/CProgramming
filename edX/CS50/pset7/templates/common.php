385492773c1d16278ddadddf60cce40d8d50a4e6
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-14 18:17:08 -0500

    textual changes.

--- edX/CS50/pset7/templates/common.php
+++ edX/CS50/pset7/templates/common.php
@@ -4,6 +4,7 @@
     <li><a href="buy.php">Buy</a></li>
     <li><a href="sell.php">Sell</a></li>
     <li><a href="history.php">History</a></li>
-    <li><a href="deposit.php">Deposit Funds</a></li>
+    <li><a href="deposit.php">Deposit Cash</a></li>
     <li><a href="logout.php"><strong>Log Out</strong></a></li>
+    <user> Login as Prasanna </user>user>
 </ul>
