bba9d7ae90fe9aebbf318b54f52277e8665ab2e6
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-14 13:44:56 -0500

    cosmic changes.

--- edX/CS50/pset7/templates/quote_price.php
+++ edX/CS50/pset7/templates/quote_price.php
@@ -1,11 +1,3 @@
-<ul class="nav nav-pills">
-    <li><a href="index.php">Portfolio</a></li>
-    <li><a href="quote.php">Quote</a></li>
-    <li><a href="buy.php">Buy</a></li>
-    <li><a href="sell.php">Sell</a></li>
-    <li><a href="history.php">History</a></li>
-    <li><a href="deposit.php">Deposit Funds</a></li>    
-    <li><a href="logout.php"><strong>Log Out</strong></a></li>
-</ul>
+include "common.php"
 
 Today's value of <?=$_POST["name"]?> (<?=$_POST["symbol"]?>) is <strong>$<?=number_format($_POST["price"], 2)?></strong>
