5b9d5c9c248745a9794fed908ea397a9a6662c04
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-15 18:41:37 -0500

    validator errors fixed.

--- edX/CS50/pset7/templates/portfolio.php
+++ edX/CS50/pset7/templates/portfolio.php
@@ -26,9 +26,12 @@ include 'common.php';
         echo("</tr>");
     }
     $total = number_format($total, 2);
+    $balance = number_format($users[0]["cash"], 2);
     echo("<tr>");
     echo("<td colspan=4>" . "Grand Total" . "</td>");
     echo("<td colspan=1>" . $total . "</td>");
+    echo("<td colspan=4>" . "Balance Cash" . "</td>");
+    echo("<td colspan=1>" . $balance . "</td>");
     echo("</tr>");
     ?>
     </tbody>
