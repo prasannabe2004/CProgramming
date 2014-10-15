20800020e5c0fddb00fbd84dbede2813611b9cf6
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-15 14:05:25 -0500

    textual changes.

--- edX/CS50/pset7/public/buy.php
+++ edX/CS50/pset7/public/buy.php
@@ -6,32 +6,22 @@
     // if form is submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        // if symbol or shares empty
         if (empty($_POST["symbol"]) || empty($_POST["shares"]))
         {
             apologize("You must enter a stock symbol and quantity of shares to buy.");
         }
-        
-        // if symbol is invalid
-        if (lookup($_POST["symbol"]) === false)
+        $stock = lookup($_POST["symbol"]);
+        if ($stock === false)
         {
             apologize("Invalid stock symbol.");
         }
-        
-        // if shares is invalid (not a whole positive integer)
         if (preg_match("/^\d+$/", $_POST["shares"]) == false)
         {
             apologize("You must enter a whole, positive integer.");
         }
-
         $transaction = 'BUY';
-
-        $stock = lookup($_POST["symbol"]);
-
         $cost = $stock["price"] * $_POST["shares"];
-
-        $cash =	query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);	
-
+        $cash =	query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
         // thanks to dump to figure out the comparision
         if ($cash[0]["cash"] < $cost)
         {
@@ -40,17 +30,9 @@
         else
         {
             $_POST["symbol"] = strtoupper($_POST["symbol"]);
-                         
-            // add stock to their portfolio or update shares
-            query("INSERT INTO portfolio (id, symbol, shares) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", $_SESSION["id"], $_POST["symbol"], $_POST["shares"]);
-            
-            // subtract total price from their cash
             query("UPDATE users SET cash = cash - ? WHERE id = ?", $cost, $_SESSION["id"]);
-            
-            // update history
+            query("INSERT INTO portfolio (id, symbol, shares) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", $_SESSION["id"], $_POST["symbol"], $_POST["shares"]);
             query("INSERT INTO history (id, transaction, symbol, shares, price) VALUES (?, ?, ?, ?, ?)", $_SESSION["id"], $transaction, $_POST["symbol"], $_POST["shares"], $stock["price"]);
-
-            //redirect to portfolio
             redirect("/");    
         }
     }
--- edX/CS50/pset7/public/sell.php
+++ edX/CS50/pset7/public/sell.php
@@ -5,19 +5,12 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
     {
         $transaction = 'SELL';
-
         $stock = lookup($_POST["symbol"]);
-
         $shares = query("SELECT shares FROM portfolio WHERE id = ? AND symbol = ?", $_SESSION["id"], $_POST["symbol"]);
-
         $value = $stock["price"] * $shares[0]["shares"];
-
         query("UPDATE users SET cash = cash + ? WHERE id = ?", $value, $_SESSION["id"]);
-
-        query("DELETE FROM portfolio WHERE id = ? AND symbol = ?", $_SESSION["id"], $_POST["symbol"]);        
-
+        query("DELETE FROM portfolio WHERE id = ? AND symbol = ?", $_SESSION["id"], $_POST["symbol"]);
         query("INSERT INTO history (id, transaction, symbol, shares, price) VALUES (?, ?, ?, ?, ?)", $_SESSION["id"], $transaction, $_POST["symbol"], $shares[0]["shares"], $stock["price"]);
-
         redirect("/");
     }
     else
