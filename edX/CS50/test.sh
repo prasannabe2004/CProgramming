baf871623dd8a6dd7ad9008b01b1bac478e9b14c
prasannabe2004 <prasannabe2004@gmail.com>
2014-10-17 16:43:48 -0400

    Adding pset8 distribution code.

--- edX/CS50/test.sh
+++ edX/CS50/test.sh
@@ -8,6 +8,7 @@ check50 2014/x/pset2/vigenere vigenere.c
 #wget http://cdn.cs50.net/2013/fall/psets/4/pset4/pset4.zip
 #wget http://cdn.cs50.net/2013/fall/psets/5/pset5/pset5.zip
 #wget http://cdn.cs50.net/2013/fall/psets/6/pset6/pset6.zip
+#wget http://cdn.cs50.net/2013/fall/psets/8/pset8/pset8.zip
 
 Find the minimum value in the array
 Swap the minimum value with the value in the first position
